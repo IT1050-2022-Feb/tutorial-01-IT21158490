c2f567d2800e6095a7fd4983973d0a2a470470a6
AJ <99630794+IT21158490@users.noreply.github.com>
2022-02-22 06:34:57 +0000

    tute 02 completed

--- Tute02.c
+++ Tute02.c
@@ -19,6 +19,22 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 #include <stdio.h>
 
 int main() {
-  
+  int d; // getting inputs for distance
+  int amount; 
+
+  printf("\nEnter the distance : ");
+  scanf("%d", &d);
+
+  if (d>20){
+    amount = 30*50 + (d-30)*40;
+  }
+  else{
+    amount = d*50;
+  }
+
+  printf("\nYou have travelled %d kilometers", d);
+  printf("\n\nYour bill is %d Rupees\n\n", amount);
+
+  printf("Thankyou !");
   return 0;
 }
