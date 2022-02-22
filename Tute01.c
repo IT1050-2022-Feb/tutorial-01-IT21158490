2fca41970094fe98eaf34e2d50f1983bf6b6742a
AJ <99630794+IT21158490@users.noreply.github.com>
2022-02-22 06:22:26 +0000

    tute 01 completed

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,25 @@
 #include <stdio.h>
 
 int main() {
+
+  printf("\n");
+  int i; // counter
+  int mark;
+  int total=0;
+  float average ;
+
+  for (i=1; i<=2; i++)
+  {
+    printf("Enter Subject %d : " ,i);
+    scanf("%d", &mark);
+
+    total = total + mark;     
+  }
+
+  average = total/(i-1);
+
+  printf("\nTotal mark is : %d \n", total);
+  printf("Average is : %f", average);
   
   return 0;
 }
