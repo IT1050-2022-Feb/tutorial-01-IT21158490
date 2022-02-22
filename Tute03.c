b5c9cb1d8b2c613dcc0fdfb6c8c50aa6dd883e15
AJ <99630794+IT21158490@users.noreply.github.com>
2022-02-22 06:56:26 +0000

    tute 03 completed

--- Tute03.c
+++ Tute03.c
@@ -13,6 +13,24 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
+  int i;
+  int n; // number to calculate untill
+  int total=0;
+
+  printf("\nLet's calculate sum from 1 to N ! ");
+
+  printf("\n\nEnter a number to N to stop : ");
+  scanf("%d", &n);
+
+  for (i=1; i<(n+1); i++){
+    total = total + i;
+    
+  }
+
+  if (n>7){
+    printf("\n\n1+2+3+...+%d,%d+%d = %d\n\n",n-2,n-1,n,total);
+  }
+  printf("Total = %d ", total);
   
   return 0;
 }
